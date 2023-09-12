4b8f33bbe01c67882d8ec27683d0bcc0ead8d00c
rox <raunaksahu55@gmail.com>
2023-09-12 10:31:55 +0530

    replaced localhoast url with new server url

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -21,7 +21,7 @@ const AdvertWidget = () => {
       width="100%"
       height="auto"
       alt="advert"
-      src="http://localhost:3001/assets/sponserimg.jpg"
+      src="https://mypeople-backend.onrender.com/assets/sponserimg.jpg"
       style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
     />
     <FlexBetween>
